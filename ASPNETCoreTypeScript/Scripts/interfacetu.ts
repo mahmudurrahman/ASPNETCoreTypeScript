d06470205ea4b070ed8d59f5fa1a481856c823d8
Mamhmudur rahman <babu_2008nsu@hotmail.com>
2017-10-19 17:32:37 +0600

    ts

--- ASPNETCoreTypeScript/Scripts/interfacetu.ts
+++ ASPNETCoreTypeScript/Scripts/interfacetu.ts
@@ -1,10 +1,66 @@
-﻿interface LabelledValue {
-    label: string;
+﻿interface Point {
+    readonly x: number;
+    readonly y: number;
 }
 
-function printLabel(labelledObj: LabelledValue) {
-    console.log(labelledObj.label);
+let p1: Point = { x: 20, y: 32 };
+
+
+let a: number[] = [1, 2, 3, 3, 3];
+let ro: ReadonlyArray<number> = a;
+
+
+interface SearchFunc {
+    (source: string, substring: string): boolean;
 }
 
-let myObj = { size: 10, label: "Size 10 Object" };
-printLabel(myObj);
+let mySearch: SearchFunc;
+mySearch = function (src: string, sub: string): boolean {
+    let result = src.search(sub);
+    return result > -1;
+}
+
+interface ClockConstructor {
+    new (hour: number, minute: number): ClockInterface;
+}
+interface ClockInterface {
+    tick();
+}
+
+function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+    return new ctor(hour, minute);
+}
+
+class DigitalClock implements ClockInterface {
+    constructor(h: number, m: number) { }
+    tick() {
+        console.log("beep beep");
+    }
+}
+class AnalogClock implements ClockInterface {
+    constructor(h: number, m: number) { }
+    tick() {
+        console.log("tick tock");
+    }
+}
+
+let digital = createClock(DigitalClock, 12, 17);
+let analog = createClock(AnalogClock, 7, 32);
+
+
+interface Shape {
+    color: string;
+}
+
+interface PenStroke {
+    penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
+    sideLength: number;
+}
+
+let square = <Square>{};
+square.color = "blue";
+square.sideLength = 10;
+square.penWidth = 5.0;
---
+++ ASPNETCoreTypeScript/wwwroot/js/functions.js
@@ -0,0 +1,15 @@
+var deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function () {
+        return function () {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedSuit = Math.floor(pickedCard / 13);
+            return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+        };
+    }
+};
+var cardPicker = deck.createCardPicker();
+var pickedCard = cardPicker();
+alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+//# sourceMappingURL=functions.js.map
