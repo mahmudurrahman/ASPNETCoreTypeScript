de1f91b2b568fa3542fb687dda86c39f311f18e6
mahmudur-rahman1 <babu_2008nsu@yahoo.com>
2017-10-19 10:40:34 +0600

    home -

---
+++ ASPNETCoreTypeScript/Scripts/Generices.ts
@@ -0,0 +1,22 @@
+﻿
+
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+getProperty(x, "a");
+
+
+class GenericNumber<T>
+{
+    zeroValue: T;
+    add: (x: T, y: T) => T;
+}
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) { return x + y; };
+
---
+++ ASPNETCoreTypeScript/wwwroot/js/Generices.js
@@ -0,0 +1,15 @@
+function getProperty(obj, key) {
+    return obj[key];
+}
+var x = { a: 1, b: 2, c: 3, d: 4 };
+getProperty(x, "a");
+
+var GenericNumber = (function () {
+    function GenericNumber() {
+    }
+    return GenericNumber;
+}());
+var myGenericNumber = new GenericNumber();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) { return x + y; };
+//# sourceMappingURL=Generices.js.map
